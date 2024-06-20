3e67fd5617effc5965d81d1ec229c7a5e028b150
Frizons <38401883+Frizons@users.noreply.github.com>
2024-06-21 00:39:34 +0200

    add koka plant model

--- Scripts/Plant.gd
+++ Scripts/Plant.gd
@@ -1,36 +1,40 @@
 extends Node3D
 
-const id = "plant"
+const id := "plant"
 
 @export var plant : Plant
 
-var grown = true
-var player_reference
+var grown := true
+var player_reference : Object
+var mesh : Object
+var component_model : Object
 
 @onready var harvest_timer := $Harvest
 @onready var grow_timer := $Grow
-@onready var fruit_model := $FruitModel
 
-func _ready():
+func _ready() -> void:
+	mesh = plant.mesh_model.instantiate()
+	component_model = mesh.get_child(1)
+	add_child(mesh)
 	grow_timer.wait_time = plant.grow_time
 	harvest_timer.wait_time = plant.harvest_time
 
-func start_harvesting(player : Object):
+func start_harvesting(player : Object) -> void:
 	player_reference = player
 	harvest_timer.start()
 
-func stop_harvesting():
+func stop_harvesting() -> void:
 	harvest_timer.stop()
 
-func _on_harvest_timeout():
+func _on_harvest_timeout() -> void:
 	player_reference.interaction_success(id, plant.harvest_components, plant.harvest_quantities)
 	if plant.vanish_on_take:
 		queue_free()
 	else:
 		grown = false
-		fruit_model.set_visible(false)
+		component_model.set_visible(false)
 		grow_timer.start()
 
-func _on_grow_timeout():
+func _on_grow_timeout() -> void:
 	grown = true
-	fruit_model.set_visible(true)
+	component_model.set_visible(true)
